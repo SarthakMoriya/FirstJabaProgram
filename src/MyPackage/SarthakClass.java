5b7347db9e0fa5b54f400b0e7ac27c15b8a0600b
Sarthak30-03-2003 <sarthak8544@gmail.com>
2023-01-08 13:49:39 +0530

    till65

---
+++ src/MyPackage/SarthakClass.java
@@ -0,0 +1,11 @@
+package MyPackage;
+
+
+public class SarthakClass {
+    public static  void display(){
+        System.out.println("Happy birthday!");
+    }
+    public static void main(String[] args) {
+        System.out.println("HEllo Sarthak class");
+    }
+}
